2344c64db0071acbfb6aa9d85cde79f4a5c66e34
Lucas Lyu <lvxl595@gmail.com>
2025-01-08 11:09:01 -0800

    feat(main): use f64 and cast i32 into it

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let mut sum = 0;
+    let mut sum = 0.0;
     for i in 0..5 {
         let even_odd = if i & 1 == 0 { "even" } else { "odd" };
         println!("{} {}", even_odd, i);
-        sum += i;
+        sum += i as f64;
     }
     println!("Sum: {}", sum);
 }
