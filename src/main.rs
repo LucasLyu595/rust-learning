5c916b413564765acabd63a0a9609f80792c8d6d
Lucas Lyu <lvxl595@gmail.com>
2025-01-08 11:01:26 -0800

    feat(main): add mutable variable sum

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
+    let mut sum = 0;
     for i in 0..5 {
-        let even_odd = if i & 1 == 0 {"even"} else {"odd"};
+        let even_odd = if i & 1 == 0 { "even" } else { "odd" };
         println!("{} {}", even_odd, i);
+        sum += i;
     }
+    println!("Sum: {}", sum);
 }
