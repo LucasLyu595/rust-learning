45c4afabba62eb05eabfea6b0a61f9d307af0865
Lucas Lyu <lvxl595@gmail.com>
2025-01-09 11:02:15 -0800

    feat(utils/vectors): add vector modification

--- src/utils/vectors.rs
+++ src/utils/vectors.rs
@@ -5,14 +5,41 @@ fn dump(arr: &[i32]) {
 pub fn run() {
     // Vector
     // re-sizeable array, after being declared as mutable
-    let v = vec![1, 2, 3];
+    let mut v1 = vec![1, 2, 3, 4];
 
-    dump(&v);
+    dump(&v1);
 
-    let first = v[0];
-    let maybe_first = v.first();
+    let first = v1[0];
+    let maybe_first = v1.first();
 
-    println!("v is {:?}", v);
+    println!("v1 is {:?}", v1);
     println!("first is {}", first);
     println!("maybe_first is {:?}", maybe_first);
+
+    // remove values from the end of a vector
+    v1.pop();
+
+    let mut v2 = Vec::new();
+    v2.push(1);
+    v2.push(2);
+    v2.push(3);
+    // compare with each other and with slices by value
+    assert_eq!(v1, v2);
+
+    // extend a vector using any compatible iterator
+    v2.extend(0..2);
+    println!("v2 is {:?}", v2);
+
+    // insert values and remove values at arbitrary positions with `insert` and `remove`
+    // not as efficient as pushing and popping
+
+    // vectors have a size and a capacity
+    // if you `clear` a vector, its size become zero, but capacity remains the same
+
+    // vectors can be sorted, and then duplicates can be removed, operated in-pkace
+    // if you want to make a copy first, use `clone` or `to_vec`
+    let mut v3 = vec![1, 10, 5, 1, 2, 11, 2, 40];
+    v3.sort();
+    v3.dedup();
+    println!("v3 is {:?}", v3);
 }
