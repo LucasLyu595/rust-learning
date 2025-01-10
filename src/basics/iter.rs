f65fb699d840bbc0481d11243a2e9e793f30bd09
Lucas Lyu <lvxl595@gmail.com>
2025-01-10 12:56:28 -0800

    feat(basics/iter) add iterator over floating-point

--- src/basics/iter.rs
+++ src/basics/iter.rs
@@ -1,3 +1,30 @@
+struct FRange {
+    val: f64,
+    end: f64,
+    incr: f64,
+}
+
+fn range(x1: f64, x2: f64, incr: f64) -> FRange {
+    FRange {
+        val: x1,
+        end: x2,
+        incr,
+    }
+}
+
+impl Iterator for FRange {
+    type Item = f64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let res = self.val;
+        if res >= self.end {
+            None
+        } else {
+            self.val += self.incr;
+            Some(res)
+        }
+    }
+}
 pub fn run() {
     // similar to the Python3 `range` function
     let mut iters = 0..3;
@@ -40,4 +67,12 @@ pub fn run() {
         print!("{:?} ", s);
     }
     println!();
+
+    for x in range(0.0, 1.0, 0.1) {
+        print!("{:.1} ", x);
+        // formatting here means one decimal after dot
+    }
+    println!();
+    // let v: Vec<f64> = range(0.0, 1.0, 0.1).map(|x| x.sin()).collect();
+    // println!("{:?}", v);
 }
